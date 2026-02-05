be93842056e842ed614c67e4c3958f74f08f7fb8
Frederic Castro <fred.cst@icloud.com>
2026-02-05 16:24:33 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,64 @@
+public function print_dossier_for_zip(FiDossier $dossier, $user)
+{
+    // 1. Iniciar TBS como lo hace tu app
+    $this->tbs = new clsTinyButStrong();
+    $this->tbs->Plugin(TBS_INSTALL, OPENTBS_PLUGIN);
+
+    // 2. Cargar misma plantilla
+    $template = TemplateManagement::DOSSIER_PATH;
+
+    $this->tbs->LoadTemplate($template, OPENTBS_ALREADY_XML);
+    $this->tbs->PlugIn(OPENTBS_DELETE_COMMENTS);
+
+    // 3. Obtener datos reales
+    $blockDossier = $this->getDossierData($dossier, $user);
+
+    $dataToMerge = array(
+        $blockDossier,
+    );
+
+    // 4. Merge idéntico al original
+    $this->tbs->MergeBlock('d', $dataToMerge);
+
+    if ($this->tbs->Plugin(OPENTBS_FILEEXISTS, 'word/header1.xml')) {
+
+        $this->tbs->Plugin(OPENTBS_SELECT_HEADER);
+
+        foreach ($blockDossier as $field => $val) {
+            $this->tbs->MergeField($field, $val);
+        }
+    }
+
+    // 5. 👉 AQUÍ LA DIFERENCIA CLAVE:
+    // Devolver DOCX en memoria
+    return $this->tbs->Show(OPENTBS_STRING);
+}
+
+
+foreach ($dossierList as $dossier) {
+
+    $folderName = $this->normalize($dossier->getTitre());
+
+    // 👉 USAMOS NUEVO MÉTODO
+    $fileContent = $this->print_dossier_for_zip($dossier, $user);
+
+    $fileName = $folderName.'.docx';
+
+    $routePrint = sys_get_temp_dir().'/'.$fileName;
+
+    file_put_contents($routePrint, $fileContent);
+
+    $tempFiles[] = [
+        PCLZIP_ATT_FILE_NAME => $routePrint,
+        PCLZIP_ATT_FILE_NEW_FULL_NAME =>
+            $folderName.'/'.$fileName
+    ];
+}
+
+
+
+
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
